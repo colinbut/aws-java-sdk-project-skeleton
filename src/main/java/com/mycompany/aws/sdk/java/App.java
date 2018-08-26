4629a454adc6d6b7f93acb74dafc51aa89c6dd41
Colin But <colinbut@users.noreply.github.com>
2018-08-26 09:56:19 +0100

    Generated source files

---
+++ src/main/java/com/mycompany/aws/sdk/java/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.aws.sdk.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
